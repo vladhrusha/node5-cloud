60953f41fb37c06057aee84256e61b50be0f8235
Vlad Hrusha <hrushavladyslavwork@gmail.com>
2023-06-06 18:16:41 +0300

    small refactoring

--- index.mjs
+++ index.mjs
@@ -1,37 +1,36 @@
-import express from "express";
 import fetch from "node-fetch";
+
 import dotenv from "dotenv";
-
 dotenv.config();
-const app = express();
-app.set("trust proxy", true);
-
-app.get("/", (req, res) => {
-  res.send("Hello, Lambda!");
-});
 
 export const handler = async (event, context) => {
-  const server = app.listen(3000, () => {});
+  try {
+    const sourceIp = event.requestContext.http.sourceIp;
+    const url = process.env.geoIpfy_URL;
+    const geoResponse = await fetch(`${url}${sourceIp}`);
+    if (!geoResponse.ok) {
+      throw new Error("Geolocation request failed");
+    }
+    const sourceGeoData = await geoResponse.json();
+    const lat = sourceGeoData.location.lat;
+    const lng = sourceGeoData.location.lng;
 
-  const userip = event.requestContext.http.sourceIp;
-  const url =
-    "https://ip-geolocation.whoisxmlapi.com/api/v1?apiKey=at_hnqocVbdqNjbI76eTam4VPViLZtcm&ipAddress=";
-  const response = await fetch(`${url}${userip}`);
-  if (!response.ok) {
-    throw new Error("Geolocation request failed");
+    const openWeatherMapKey = process.env.openWeatherToken;
+    const fetchURL = `${process.env.openWeather_URL}?lat=${lat}&lon=${lng}&appid=${openWeatherMapKey}&units=metric&cnt=7`;
+    const weatherResponse = await fetch(fetchURL);
+    if (!weatherResponse.ok) {
+      throw new Error("Forecast request failed");
+    }
+    const forecastResult = await weatherResponse.json();
+
+    return {
+      statusCode: 200,
+      body: forecastResult,
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: err.message,
+    };
   }
-  const data = await response.json();
-  const lat = data.location.lat;
-  const lng = data.location.lng;
-  const openWeatherMapKey = process.env.openWeatherToken;
-  const fetchURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lng}&appid=${openWeatherMapKey}&units=metric&cnt=7`;
-  const weatherResponse = await fetch(fetchURL);
-  const result = await weatherResponse.json();
-  context.callbackWaitsForEmptyEventLoop = false;
-  server.close();
-
-  return {
-    statusCode: 200,
-    body: { result },
-  };
 };
